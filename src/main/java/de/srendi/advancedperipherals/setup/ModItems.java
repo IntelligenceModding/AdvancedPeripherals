8038a2fc4ee770218aaab9beba9a1dd44f2fd055
Seniorendi <info@srendi.de>
2020-11-09 15:09:40 +0100

    Fixed typo in silver_ingot

--- src/main/java/de/srendi/advancedperipherals/setup/ModItems.java
+++ src/main/java/de/srendi/advancedperipherals/setup/ModItems.java
@@ -6,7 +6,7 @@ import net.minecraftforge.fml.RegistryObject;
 
 
 public class ModItems {
-    public static final RegistryObject<Item> SILVER_INGOT = Registration.ITEMS.register("siver_ingot", () ->
+    public static final RegistryObject<Item> SILVER_INGOT = Registration.ITEMS.register("silver_ingot", () ->
             new Item(new Item.Properties().group(ItemGroup.MATERIALS).maxDamage(200000)));
 
     static void register() {
