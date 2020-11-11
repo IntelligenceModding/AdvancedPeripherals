a0271549114c049de09a1aa0f3bfe8c427401fe0
Seniorendi <info@srendi.de>
2020-11-11 16:56:52 +0100

    Added cc:tweaked api

--- src/main/java/de/srendi/advancedperipherals/blocks/SilverOre.java
+++ src/main/java/de/srendi/advancedperipherals/blocks/SilverOre.java
@@ -2,8 +2,11 @@ package de.srendi.advancedperipherals.blocks;
 
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorldReader;
 import net.minecraftforge.common.ToolType;
 
 public class SilverOre extends Block {
@@ -15,6 +18,11 @@ public class SilverOre extends Block {
                 .sound(SoundType.STONE)
                 .harvestTool(ToolType.PICKAXE)
                 .setRequiresTool());
+
     }
 
+    @Override
+    public int getExpDrop(BlockState state, IWorldReader world, BlockPos pos, int fortune, int silktouch) {
+        return 0;
+    }
 }
