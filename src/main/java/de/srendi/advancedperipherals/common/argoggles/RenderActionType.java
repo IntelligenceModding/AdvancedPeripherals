8083fef596e9a02cb172e832076906d5f48eea60
Olfi01 <flom@meyer-alpers.de>
2021-04-25 17:35:10 +0200

    Added DrawCircle, FillCircle, DrawItemIcon

--- src/main/java/de/srendi/advancedperipherals/common/argoggles/RenderActionType.java
+++ src/main/java/de/srendi/advancedperipherals/common/argoggles/RenderActionType.java
@@ -4,20 +4,21 @@ import de.srendi.advancedperipherals.AdvancedPeripherals;
 
 public enum RenderActionType {
 
-    DrawCenteredString(3), DrawString(3), Fill(5), HorizontalLine(4), VerticalLine(4), FillGradient(6), DrawRightboundString(3);
+	DrawCenteredString(3), DrawString(3), Fill(5), HorizontalLine(4), VerticalLine(4), FillGradient(6),
+	DrawRightboundString(3), DrawCircle(4), FillCircle(4), DrawItemIcon(2);
 
-    int intArgCount;
+	int intArgCount;
 
-    RenderActionType(int intArgCount) {
-        this.intArgCount = intArgCount;
-    }
+	RenderActionType(int intArgCount) {
+		this.intArgCount = intArgCount;
+	}
 
-    boolean ensureArgs(int[] args) {
-        boolean correct = args.length >= intArgCount;
-        if (!correct)
-            AdvancedPeripherals.LOGGER.warn(
-                    "Got invalid number of arguments for AR render action {}: expected {}, got {}", this.toString(),
-                    intArgCount, args.length);
-        return correct;
-    }
+	boolean ensureArgs(int[] args) {
+		boolean correct = args.length >= intArgCount;
+		if (!correct)
+			AdvancedPeripherals.LOGGER.warn(
+					"Got invalid number of arguments for AR render action {}: expected {}, got {}", this.toString(),
+					intArgCount, args.length);
+		return correct;
+	}
 }
