1edf4fa16ec5161cac28ffd8127eb2bc87494282
henkeeman <55483865+henkeeman@users.noreply.github.com>
2021-06-10 19:18:35 +0200

    Updated VillagerTrades.java Raised Prices
    I am currently making a custom modpack and I noticed that the computer scientist prices were a bit too low imo. It's completely fine if you liked the old values but I would greatly appreciate a config option to disable the computer scientist profession in that case. Cheers!

--- src/main/java/de/srendi/advancedperipherals/common/village/VillagerTrades.java
+++ src/main/java/de/srendi/advancedperipherals/common/village/VillagerTrades.java
@@ -21,10 +21,10 @@ public class VillagerTrades {
     @SubscribeEvent
     public static void registerWanderingTrade(WandererTradesEvent event) {
         event.getGenericTrades().add(new VillagerTrade(VillagerTrade.Type.ItemForEmerald).setItem(Blocks.PERIPHERAL_CASING.get())
-                .setEmeraldPrice(1).setMaxUses(16).setXp(3));
+                .setEmeraldPrice(7).setMaxUses(16).setXp(3));
 
         event.getRareTrades().add(new VillagerTrade(VillagerTrade.Type.EmeraldForItem).setItem(Registry.ModBlocks.TURTLE_ADVANCED.get())
-                .setEmeraldPrice(2).setMaxUses(4).setXp(20).setItemAmount(1));
+                .setEmeraldPrice(34).setMaxUses(4).setXp(20).setItemAmount(1));
     }
 
     @SubscribeEvent
@@ -33,54 +33,54 @@ public class VillagerTrades {
         if (event.getType() == Villagers.COMPUTER_SCIENTIST.get()) {
 
             trades.get(1).add(new VillagerTrade(VillagerTrade.Type.ItemForEmerald).setItem(Blocks.PERIPHERAL_CASING.get())
-                    .setEmeraldPrice(2).setMaxUses(10).setXp(4));
+                    .setEmeraldPrice(10).setMaxUses(10).setXp(4));
             trades.get(1).add(new VillagerTrade(VillagerTrade.Type.ItemForEmerald).setItem(Blocks.CHAT_BOX.get())
-                    .setEmeraldPrice(2).setMaxUses(8).setXp(4));
+                    .setEmeraldPrice(22).setMaxUses(8).setXp(4));
             trades.get(1).add(new VillagerTrade(VillagerTrade.Type.ItemForEmerald).setItem(Registry.ModBlocks.COMPUTER_NORMAL.get())
-                    .setEmeraldPrice(1).setMaxUses(8).setXp(4));
+                    .setEmeraldPrice(17).setMaxUses(8).setXp(4));
             trades.get(1).add(new VillagerTrade(VillagerTrade.Type.EmeraldForItem).setItem(Blocks.PERIPHERAL_CASING.get())
-                    .setEmeraldPrice(1).setMaxUses(8).setXp(2));
+                    .setEmeraldPrice(10).setMaxUses(8).setXp(2));
 
             trades.get(2).add(new VillagerTrade(VillagerTrade.Type.ItemForEmerald).setItem(Blocks.INVENTORY_MANAGER.get())
-                    .setEmeraldPrice(2).setMaxUses(2).setXp(8));
+                    .setEmeraldPrice(20).setMaxUses(2).setXp(8));
             trades.get(2).add(new VillagerTrade(VillagerTrade.Type.ItemForEmerald).setItem(Blocks.REDSTONE_INTEGRATOR.get())
-                    .setEmeraldPrice(2).setMaxUses(2).setXp(8));
+                    .setEmeraldPrice(16).setMaxUses(2).setXp(8));
             trades.get(2).add(new VillagerTrade(VillagerTrade.Type.ItemForEmerald).setItem(Registry.ModBlocks.COMPUTER_ADVANCED.get())
-                    .setEmeraldPrice(2).setMaxUses(3).setXp(10));
+                    .setEmeraldPrice(31).setMaxUses(3).setXp(10));
             trades.get(2).add(new VillagerTrade(VillagerTrade.Type.ItemForEmerald).setItem(Registry.ModBlocks.MONITOR_NORMAL.get())
-                    .setEmeraldPrice(1).setMaxUses(8).setXp(6).setItemAmount(8));
+                    .setEmeraldPrice(11).setMaxUses(8).setXp(6).setItemAmount(8));
 
             trades.get(3).add(new VillagerTrade(VillagerTrade.Type.ItemForEmerald).setItem(ItemUtil.makeTurtle(ItemUtil.TURTLE_NORMAL, AdvancedPeripherals.MOD_ID + ":environment_detector_turtle"))
-                    .setEmeraldPrice(3).setMaxUses(4).setXp(10));
+                    .setEmeraldPrice(33).setMaxUses(4).setXp(10));
             trades.get(3).add(new VillagerTrade(VillagerTrade.Type.ItemForEmerald).setItem(ItemUtil.makePocket(ItemUtil.POCKET_NORMAL, AdvancedPeripherals.MOD_ID + ":player_pocket"))
-                    .setEmeraldPrice(2).setMaxUses(4).setXp(10));
+                    .setEmeraldPrice(31).setMaxUses(4).setXp(10));
             trades.get(3).add(new VillagerTrade(VillagerTrade.Type.EmeraldForItem).setItem(Items.CHUNK_CONTROLLER.get())
-                    .setEmeraldPrice(1).setMaxUses(6).setXp(8));
+                    .setEmeraldPrice(49).setMaxUses(6).setXp(8));
             trades.get(3).add(new VillagerTrade(VillagerTrade.Type.ItemForEmerald).setItem(Items.COMPUTER_TOOL.get())
-                    .setEmeraldPrice(1).setMaxUses(4).setXp(8));
+                    .setEmeraldPrice(7).setMaxUses(4).setXp(8));
             trades.get(3).add(new VillagerTrade(VillagerTrade.Type.ItemForEmerald).setItem(Registry.ModBlocks.MONITOR_ADVANCED.get())
-                    .setEmeraldPrice(6).setMaxUses(8).setXp(10).setItemAmount(16));
+                    .setEmeraldPrice(23).setMaxUses(8).setXp(10).setItemAmount(16));
 
             trades.get(4).add(new VillagerTrade(VillagerTrade.Type.ItemForEmerald).setItem(Blocks.ENERGY_DETECTOR.get())
-                    .setEmeraldPrice(3).setMaxUses(8).setXp(12));
+                    .setEmeraldPrice(28).setMaxUses(8).setXp(12));
             trades.get(4).add(new VillagerTrade(VillagerTrade.Type.EmeraldForItem).setItem(Registry.ModBlocks.COMPUTER_ADVANCED.get())
-                    .setEmeraldPrice(2).setMaxUses(8).setXp(12));
+                    .setEmeraldPrice(34).setMaxUses(8).setXp(12));
             trades.get(4).add(new VillagerTrade(VillagerTrade.Type.ItemForEmerald).setItem(Items.MEMORY_CARD.get())
-                    .setEmeraldPrice(1).setMaxUses(8).setXp(18));
+                    .setEmeraldPrice(9).setMaxUses(8).setXp(18));
             trades.get(4).add(new VillagerTrade(VillagerTrade.Type.EmeraldForItem).setItem(Registry.ModItems.PRINTED_PAGE.get())
-                    .setEmeraldPrice(1).setMaxUses(10).setXp(16).setItemAmount(4));
+                    .setEmeraldPrice(2).setMaxUses(10).setXp(16).setItemAmount(4));
             trades.get(4).add(new VillagerTrade(VillagerTrade.Type.ItemForEmerald).setItem(ItemUtil.makeTurtle(ItemUtil.TURTLE_ADVANCED, AdvancedPeripherals.MOD_ID + ":chunky_turtle"))
-                    .setEmeraldPrice(8).setMaxUses(6).setXp(18));
+                    .setEmeraldPrice(64).setMaxUses(6).setXp(18));
 
             trades.get(5).add(new VillagerTrade(VillagerTrade.Type.ItemForEmerald).setItem(Blocks.AR_CONTROLLER.get())
-                    .setEmeraldPrice(3).setMaxUses(6).setXp(30));
+                    .setEmeraldPrice(29).setMaxUses(6).setXp(30));
             trades.get(5).add(new VillagerTrade(VillagerTrade.Type.ItemForEmerald).setItem(Items.AR_GOGGLES.get())
-                    .setEmeraldPrice(4).setMaxUses(8).setXp(30));
+                    .setEmeraldPrice(7).setMaxUses(8).setXp(30));
             if (ModList.get().isLoaded("appliedenergistics2"))
                 trades.get(5).add(new VillagerTrade(VillagerTrade.Type.ItemForEmerald).setItem(Blocks.ME_BRIDGE.get())
-                        .setEmeraldPrice(3).setMaxUses(10).setXp(18));
+                        .setEmeraldPrice(64).setMaxUses(10).setXp(18));
             trades.get(5).add(new VillagerTrade(VillagerTrade.Type.EmeraldForItem).setItem(Registry.ModBlocks.WIRELESS_MODEM_ADVANCED.get())
-                    .setEmeraldPrice(2).setMaxUses(14).setXp(30));
+                    .setEmeraldPrice(43).setMaxUses(14).setXp(30));
 
         }
     }
