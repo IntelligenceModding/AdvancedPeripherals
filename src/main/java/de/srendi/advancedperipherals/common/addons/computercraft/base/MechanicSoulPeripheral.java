48d03da70fdd5402f3265432a1a56d2ab15f4c16
SirEdvin <siredvin.dark@gmail.com>
2021-07-11 23:06:55 +0300

    Another API fix

--- src/main/java/de/srendi/advancedperipherals/common/addons/computercraft/base/MechanicSoulPeripheral.java
+++ src/main/java/de/srendi/advancedperipherals/common/addons/computercraft/base/MechanicSoulPeripheral.java
@@ -11,8 +11,8 @@ public abstract class MechanicSoulPeripheral extends FuelConsumingPeripheral {
 
     // So, we storing here turtle and side
     // Because this peripheral are only for turtles
-    private final ITurtleAccess turtle;
-    private final TurtleSide side;
+    protected final ITurtleAccess turtle;
+    protected final TurtleSide side;
 
     protected static final String FUEL_CONSUMING_RATE_SETTING = "FUEL_CONSUMING_RATE";
     protected static final int DEFAULT_FUEL_CONSUMING_RATE = 1;
