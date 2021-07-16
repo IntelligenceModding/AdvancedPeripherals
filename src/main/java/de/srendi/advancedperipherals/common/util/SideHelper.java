19c82affd6a29a4d7882b90536e751671e36dd7e
Sr_endi <info@denizdemiroglu.de>
2021-07-17 00:08:47 +0200

    Remove not used API methods

--- src/main/java/de/srendi/advancedperipherals/common/util/SideHelper.java
+++ src/main/java/de/srendi/advancedperipherals/common/util/SideHelper.java
@@ -5,6 +5,7 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 
 public class SideHelper {
+
     public static boolean isClientPlayer(LivingEntity player) {
         return player.level.isClientSide && player instanceof PlayerEntity && player.level.getServer() == null
                 && player == Minecraft.getInstance().player;
