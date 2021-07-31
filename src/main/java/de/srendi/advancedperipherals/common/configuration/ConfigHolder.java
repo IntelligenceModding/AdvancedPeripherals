8bbb9a3003d142e39d7a2da268df0412dcf56cbd
Sr_endi <info@denizdemiroglu.de>
2021-07-31 02:07:51 +0200

    You can now reload gradle without issues. But you can't build the mod anyways. Nice commit, not?

--- src/main/java/de/srendi/advancedperipherals/common/configuration/ConfigHolder.java
+++ src/main/java/de/srendi/advancedperipherals/common/configuration/ConfigHolder.java
@@ -4,14 +4,13 @@ import net.minecraftforge.common.ForgeConfigSpec;
 import org.apache.commons.lang3.tuple.Pair;
 
 public class ConfigHolder {
+
     public static final ForgeConfigSpec COMMON_SPEC;
     static final AdvancedPeripheralsConfig.CommonConfig COMMON_CONFIG;
 
     static {
-        {
-            final Pair<AdvancedPeripheralsConfig.CommonConfig, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(AdvancedPeripheralsConfig.CommonConfig::new);
-            COMMON_CONFIG = specPair.getLeft();
-            COMMON_SPEC = specPair.getRight();
-        }
+        final Pair<AdvancedPeripheralsConfig.CommonConfig, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(AdvancedPeripheralsConfig.CommonConfig::new);
+        COMMON_CONFIG = specPair.getLeft();
+        COMMON_SPEC = specPair.getRight();
     }
 }
