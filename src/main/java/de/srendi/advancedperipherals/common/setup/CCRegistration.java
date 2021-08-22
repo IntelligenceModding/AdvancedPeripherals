c1f4b2e50af82f7054a44bfb00325780b0dce356
SirEdvin <siredvin.dark@gmail.com>
2021-08-22 14:16:26 +0300

    Simpilify pocket and turtle upgrade registration

--- src/main/java/de/srendi/advancedperipherals/common/setup/CCRegistration.java
+++ src/main/java/de/srendi/advancedperipherals/common/setup/CCRegistration.java
@@ -1,6 +1,8 @@
 package de.srendi.advancedperipherals.common.setup;
 
 import dan200.computercraft.api.ComputerCraftAPI;
+import dan200.computercraft.api.pocket.AbstractPocketUpgrade;
+import dan200.computercraft.api.turtle.AbstractTurtleUpgrade;
 import de.srendi.advancedperipherals.AdvancedPeripherals;
 import de.srendi.advancedperipherals.common.addons.computercraft.base.IntegrationPeripheralProvider;
 import de.srendi.advancedperipherals.common.addons.computercraft.pocket.*;
@@ -8,26 +10,14 @@ import de.srendi.advancedperipherals.common.addons.computercraft.turtles.*;
 import de.srendi.advancedperipherals.common.addons.computercraft.turtles.metaphysics.*;
 import net.minecraftforge.fml.common.Mod;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Mod.EventBusSubscriber(modid = AdvancedPeripherals.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class CCRegistration {
 
-    public static TurtleChatBox chatBox;
-    public static TurtleEnvironmentDetector environmentDetector;
-    public static TurtlePlayerDetector playerDetector;
-    public static TurtleChunky chunky;
-    public static TurtleGeoScanner geoScanner;
-    public static WeakAutomata weakAutomata;
-    public static EndAutomata endAutomata;
-    public static HusbandryAutomata husbandryAutomata;
-    public static OverpoweredWeakAutomata overpoweredWeakAutomata;
-    public static OverpoweredEndAutomata overpoweredEndAutomata;
-    public static OverpoweredHusbandryAutomata overpoweredHusbandryAutomata;
-
-    public static PocketEnvironment environmentPocket;
-    public static PocketChatBox chatPocket;
-    public static PocketPlayerDetector playerPocket;
-    public static PocketGeoScanner geoScannerPocket;
-    public static PocketColonyIntegrator colonyIntegratorPocket;
+    public static List<AbstractTurtleUpgrade> turtleUpgrades;
+    public static List<AbstractPocketUpgrade> pocketUpgrades;
 
     public static IntegrationPeripheralProvider integrationPeripheralProvider;
 
@@ -40,41 +30,31 @@ public class CCRegistration {
     }
 
     private static void registerPocketUpgrades() {
-        environmentPocket = new PocketEnvironment();
-        ComputerCraftAPI.registerPocketUpgrade(environmentPocket);
-        chatPocket = new PocketChatBox();
-        ComputerCraftAPI.registerPocketUpgrade(chatPocket);
-        playerPocket = new PocketPlayerDetector();
-        ComputerCraftAPI.registerPocketUpgrade(playerPocket);
-        geoScannerPocket = new PocketGeoScanner();
-        ComputerCraftAPI.registerPocketUpgrade(geoScannerPocket);
-        colonyIntegratorPocket = new PocketColonyIntegrator();
-        ComputerCraftAPI.registerPocketUpgrade(colonyIntegratorPocket);
+        pocketUpgrades = new ArrayList<AbstractPocketUpgrade>() {{
+            add(new PocketEnvironment());
+            add(new PocketChatBox());
+            add(new PocketPlayerDetector());
+            add(new PocketGeoScanner());
+            add(new PocketColonyIntegrator());
+        }};
+        pocketUpgrades.forEach(ComputerCraftAPI::registerPocketUpgrade);
     }
 
     private static void registerTurtleUpgrades() {
-        chatBox = new TurtleChatBox();
-        ComputerCraftAPI.registerTurtleUpgrade(chatBox);
-        playerDetector = new TurtlePlayerDetector();
-        ComputerCraftAPI.registerTurtleUpgrade(playerDetector);
-        environmentDetector = new TurtleEnvironmentDetector();
-        ComputerCraftAPI.registerTurtleUpgrade(environmentDetector);
-        chunky = new TurtleChunky();
-        ComputerCraftAPI.registerTurtleUpgrade(chunky);
-        geoScanner = new TurtleGeoScanner();
-        ComputerCraftAPI.registerTurtleUpgrade(geoScanner);
-        weakAutomata = new WeakAutomata();
-        ComputerCraftAPI.registerTurtleUpgrade(weakAutomata);
-        endAutomata = new EndAutomata();
-        ComputerCraftAPI.registerTurtleUpgrade(endAutomata);
-        husbandryAutomata = new HusbandryAutomata();
-        ComputerCraftAPI.registerTurtleUpgrade(husbandryAutomata);
-        overpoweredWeakAutomata = new OverpoweredWeakAutomata();
-        ComputerCraftAPI.registerTurtleUpgrade(overpoweredWeakAutomata);
-        overpoweredEndAutomata = new OverpoweredEndAutomata();
-        ComputerCraftAPI.registerTurtleUpgrade(overpoweredEndAutomata);
-        overpoweredHusbandryAutomata = new OverpoweredHusbandryAutomata();
-        ComputerCraftAPI.registerTurtleUpgrade(overpoweredHusbandryAutomata);
+        turtleUpgrades = new ArrayList<AbstractTurtleUpgrade>() {{
+            add(new TurtleChatBox());
+            add(new TurtlePlayerDetector());
+            add(new TurtleEnvironmentDetector());
+            add(new TurtleChunky());
+            add(new TurtleGeoScanner());
+            add(new WeakAutomata());
+            add(new EndAutomata());
+            add(new HusbandryAutomata());
+            add(new OverpoweredWeakAutomata());
+            add(new OverpoweredEndAutomata());
+            add(new OverpoweredHusbandryAutomata());
+        }};
+        turtleUpgrades.forEach(ComputerCraftAPI::registerTurtleUpgrade);
     }
 
 }
