e83782aada94261174bb756ff7bb9488c43c586a
ThatGravyBoat <thatgravyboat@gmail.com>
2022-02-05 14:17:09 -0330

    Added ids to AR Renderer

--- src/main/java/de/srendi/advancedperipherals/common/addons/computercraft/peripheral/ARControllerPeripheral.java
+++ src/main/java/de/srendi/advancedperipherals/common/addons/computercraft/peripheral/ARControllerPeripheral.java
@@ -50,8 +50,18 @@ public class ARControllerPeripheral extends BasePeripheral<BlockEntityPeripheral
     }
 
     @LuaFunction(mainThread = true)
+    public final void drawStringWithId(String id, String text, int x, int y, int color) {
+        owner.tileEntity.addToCanvas(new ARRenderAction(id, RenderActionType.DrawString, text, x, y, color));
+    }
+
+    @LuaFunction(mainThread = true)
     public final void drawCenteredString(String text, int x, int y, int color) {
         owner.tileEntity.addToCanvas(new ARRenderAction(RenderActionType.DrawCenteredString, text, x, y, color));
+    }
+
+    @LuaFunction(mainThread = true)
+    public final void drawCenteredStringWithId(String id, String text, int x, int y, int color) {
+        owner.tileEntity.addToCanvas(new ARRenderAction(id, RenderActionType.DrawCenteredString, text, x, y, color));
     }
 
     @LuaFunction(mainThread = true)
@@ -60,8 +70,18 @@ public class ARControllerPeripheral extends BasePeripheral<BlockEntityPeripheral
     }
 
     @LuaFunction(mainThread = true)
+    public final void drawRightboundStringWithId(String id, String text, int x, int y, int color) {
+        owner.tileEntity.addToCanvas(new ARRenderAction(id, RenderActionType.DrawRightboundString, text, x, y, color));
+    }
+
+    @LuaFunction(mainThread = true)
     public final void horizontalLine(int minX, int maxX, int y, int color) {
         owner.tileEntity.addToCanvas(new ARRenderAction(RenderActionType.HorizontalLine, minX, maxX, y, color));
+    }
+
+    @LuaFunction(mainThread = true)
+    public final void horizontalLineWithId(String id, int minX, int maxX, int y, int color) {
+        owner.tileEntity.addToCanvas(new ARRenderAction(id, RenderActionType.HorizontalLine, minX, maxX, y, color));
     }
 
     @LuaFunction(mainThread = true)
@@ -70,8 +90,18 @@ public class ARControllerPeripheral extends BasePeripheral<BlockEntityPeripheral
     }
 
     @LuaFunction(mainThread = true)
+    public final void verticalLineWithId(String id, int x, int minY, int maxY, int color) {
+        owner.tileEntity.addToCanvas(new ARRenderAction(id, RenderActionType.VerticalLine, x, minY, maxY, color));
+    }
+
+    @LuaFunction(mainThread = true)
     public final void fill(int minX, int minY, int maxX, int maxY, int color) {
         owner.tileEntity.addToCanvas(new ARRenderAction(RenderActionType.Fill, minX, minY, maxX, maxY, color));
+    }
+
+    @LuaFunction(mainThread = true)
+    public final void fillWithId(String id, int minX, int minY, int maxX, int maxY, int color) {
+        owner.tileEntity.addToCanvas(new ARRenderAction(id, RenderActionType.Fill, minX, minY, maxX, maxY, color));
     }
 
     @LuaFunction(mainThread = true)
@@ -80,8 +110,18 @@ public class ARControllerPeripheral extends BasePeripheral<BlockEntityPeripheral
     }
 
     @LuaFunction(mainThread = true)
+    public final void fillGradientWithId(String id, int minX, int minY, int maxX, int maxY, int colorFrom, int colorTo) {
+        owner.tileEntity.addToCanvas(new ARRenderAction(id, RenderActionType.FillGradient, minX, minY, maxX, maxY, colorFrom, colorTo));
+    }
+
+    @LuaFunction(mainThread = true)
     public final void drawCircle(int x, int y, int radius, int color) {
         owner.tileEntity.addToCanvas(new ARRenderAction(RenderActionType.DrawCircle, x, y, radius, color));
+    }
+
+    @LuaFunction(mainThread = true)
+    public final void drawCircleWithId(String id, int x, int y, int radius, int color) {
+        owner.tileEntity.addToCanvas(new ARRenderAction(id, RenderActionType.DrawCircle, x, y, radius, color));
     }
 
     @LuaFunction(mainThread = true)
@@ -90,12 +130,27 @@ public class ARControllerPeripheral extends BasePeripheral<BlockEntityPeripheral
     }
 
     @LuaFunction(mainThread = true)
+    public final void fillCircleWithId(String id, int x, int y, int radius, int color) {
+        owner.tileEntity.addToCanvas(new ARRenderAction(id, RenderActionType.FillCircle, x, y, radius, color));
+    }
+
+    @LuaFunction(mainThread = true)
     public final void drawItemIcon(String itemId, int x, int y) {
         owner.tileEntity.addToCanvas(new ARRenderAction(RenderActionType.DrawItemIcon, itemId, x, y));
     }
 
     @LuaFunction(mainThread = true)
+    public final void drawItemIconWithId(String id, String itemId, int x, int y) {
+        owner.tileEntity.addToCanvas(new ARRenderAction(id, RenderActionType.DrawItemIcon, itemId, x, y));
+    }
+
+    @LuaFunction(mainThread = true)
     public final void clear() {
         owner.tileEntity.clearCanvas();
+    }
+
+    @LuaFunction(mainThread = true)
+    public final void clearElement(String id) {
+        owner.tileEntity.clearElement(id);
     }
 }
