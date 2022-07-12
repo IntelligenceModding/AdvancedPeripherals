f2a24eebab9733ed710d143826a84c938dd4002d
Deniz Demiroglu <info@denizdemiroglu.de>
2022-07-12 10:03:47 +0000

    oops

--- .space.kts
+++ .space.kts
@@ -1,11 +1,13 @@
-container("openjdk:17") {
-    kotlinScript { api ->
-        api.space().projects.automation.deployments.start(
-            project = api.projectIdentifier(),
-            targetIdentifier = TargetIdentifier.Key("ap-test-builds"),
-            version = "0.7-Alpha",
-            // automatically update deployment status based on a status of a job
-            syncWithAutomationJob = true
-        )
-    }
+job("Deploy test builds") {
+	container("openjdk:17") {
+    	kotlinScript { api ->
+        	api.space().projects.automation.deployments.start(
+            	project = api.projectIdentifier(),
+            	targetIdentifier = TargetIdentifier.Key("ap-test-builds"),
+            	version = "0.7-Alpha",
+            	// automatically update deployment status based on a status of a job
+            	syncWithAutomationJob = true
+        	)
+    	}
+	}
 }
