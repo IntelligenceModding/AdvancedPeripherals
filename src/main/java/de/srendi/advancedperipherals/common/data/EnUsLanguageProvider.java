adacb7966068fd1e68a448ab62131b57097ed4d1
Sr_endi <info@denizdemiroglu.de>
2022-09-26 22:15:48 +0200

    0.7.20a 1.19.2

--- src/main/java/de/srendi/advancedperipherals/common/data/EnUsLanguageProvider.java
+++ src/main/java/de/srendi/advancedperipherals/common/data/EnUsLanguageProvider.java
@@ -2,9 +2,16 @@ package de.srendi.advancedperipherals.common.data;
 
 import de.srendi.advancedperipherals.AdvancedPeripherals;
 import de.srendi.advancedperipherals.common.setup.Blocks;
+import de.srendi.advancedperipherals.common.setup.CCRegistration;
 import de.srendi.advancedperipherals.common.setup.Items;
+import de.srendi.advancedperipherals.common.setup.Villagers;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.npc.VillagerProfession;
 import net.minecraftforge.common.data.LanguageProvider;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.function.Supplier;
 
 public class EnUsLanguageProvider extends LanguageProvider {
 
@@ -15,47 +22,88 @@ public class EnUsLanguageProvider extends LanguageProvider {
     @Override
     protected void addTranslations() {
         addBlocks();
-        addGuis();
-        addTooltips();
-        add("itemGroup." + AdvancedPeripherals.MOD_ID, "Advanced Peripherals");
+        addItems();
+        addTurtles();
+        addPockets();
+        add(Villagers.COMPUTER_SCIENTIST, "Computer Scientist");
+        add("itemGroup." + AdvancedPeripherals.TAB.getRecipeFolderName(), "Advanced Peripherals");
     }
 
     private void addItems() {
-        add(Items.CHUNK_CONTROLLER.get(), "Chunk Controller");
-        add(Items.COMPUTER_TOOL.get(), "Computer Tool");
-        add(Items.WEAK_AUTOMATA_CORE.get(), "Weak Automata Core");
-        add(Items.HUSBANDRY_AUTOMATA_CORE.get(), "Husbandry Automata Core");
-        add(Items.END_AUTOMATA_CORE.get(), "End Automata Core");
-        add(Items.OVERPOWERED_WEAK_AUTOMATA_CORE.get(), "Overpowered Weak Automata Core");
-        add(Items.OVERPOWERED_HUSBANDRY_AUTOMATA_CORE.get(), "Overpowered Husbandry Automata Core");
-        add(Items.OVERPOWERED_END_AUTOMATA_CORE.get(), "Overpowered End Automata Core");
-        add(Items.MEMORY_CARD.get(), "Memory Card");
+        addItem(Items.CHUNK_CONTROLLER, "Chunk Controller");
+        addItem(Items.COMPUTER_TOOL, "Computer Tool");
+        addItem(Items.WEAK_AUTOMATA_CORE, "Weak Automata Core");
+        addItem(Items.HUSBANDRY_AUTOMATA_CORE, "Husbandry Automata Core");
+        addItem(Items.END_AUTOMATA_CORE, "End Automata Core");
+        addItem(Items.OVERPOWERED_WEAK_AUTOMATA_CORE, "Overpowered Weak Automata Core");
+        addItem(Items.OVERPOWERED_HUSBANDRY_AUTOMATA_CORE, "Overpowered Husbandry Automata Core");
+        addItem(Items.OVERPOWERED_END_AUTOMATA_CORE, "Overpowered End Automata Core");
+        addItem(Items.MEMORY_CARD, "Memory Card");
     }
 
     private void addBlocks() {
-        add(Blocks.BLOCK_READER.get(), "Block Reader");
-        add(Blocks.CHAT_BOX.get(), "Chat Box");
-        add(Blocks.AR_CONTROLLER.get(), "Ar Controller");
-        add(Blocks.COLONY_INTEGRATOR.get(), "Colony Integrator");
-        add(Blocks.ENERGY_DETECTOR.get(), "Energy Detector");
-        add(Blocks.ENVIRONMENT_DETECTOR.get(), "Environment Detector");
-        add(Blocks.GEO_SCANNER.get(), "Geo Scanner");
-        add(Blocks.INVENTORY_MANAGER.get(), "Inventory Manager");
-        add(Blocks.ME_BRIDGE.get(), "ME Bridge");
-        add(Blocks.RS_BRIDGE.get(), "RS Bridge");
-        add(Blocks.NBT_STORAGE.get(), "NBT Storage");
-        add(Blocks.PERIPHERAL_CASING.get(), "Peripheral Casing");
-        add(Blocks.PLAYER_DETECTOR.get(), "Player Detector");
-        add(Blocks.REDSTONE_INTEGRATOR.get(), "Redstone Integrator");
+        addBlock(Blocks.BLOCK_READER, "Block Reader");
+        addBlock(Blocks.CHAT_BOX, "Chat Box");
+        addBlock(Blocks.AR_CONTROLLER, "Ar Controller");
+        addBlock(Blocks.COLONY_INTEGRATOR, "Colony Integrator");
+        addBlock(Blocks.ENERGY_DETECTOR, "Energy Detector");
+        addBlock(Blocks.ENVIRONMENT_DETECTOR, "Environment Detector");
+        addBlock(Blocks.GEO_SCANNER, "Geo Scanner");
+        addBlock(Blocks.INVENTORY_MANAGER, "Inventory Manager");
+        addBlock(Blocks.ME_BRIDGE, "ME Bridge");
+        addBlock(Blocks.RS_BRIDGE, "RS Bridge");
+        addBlock(Blocks.NBT_STORAGE, "NBT Storage");
+        addBlock(Blocks.PERIPHERAL_CASING, "Peripheral Casing");
+        addBlock(Blocks.PLAYER_DETECTOR, "Player Detector");
+        addBlock(Blocks.REDSTONE_INTEGRATOR, "Redstone Integrator");
 
     }
 
-    private void addGuis() {
+    private void addTurtles() {
+        addTurtle(CCRegistration.ID.CHUNKY_TURTLE, "Chunky");
+        addTurtle(CCRegistration.ID.CHATTY_TURTLE, "Chatty");
+        addTurtle(CCRegistration.ID.ENVIRONMENT_TURTLE, "Environment");
+        addTurtle(CCRegistration.ID.PLAYER_TURTLE, "Player Detector");
+        addTurtle(CCRegistration.ID.GEOSCANNER_TURTLE, "Geo");
+        addTurtle(CCRegistration.ID.COMPASS_TURTLE, "Compass");
+        addTurtle(CCRegistration.ID.WEAK_AUTOMATA, "Weak automata");
+        addTurtle(CCRegistration.ID.HUSBANDRY_AUTOMATA, "Husbandry automata");
+        addTurtle(CCRegistration.ID.END_AUTOMATA, "End automata");
+        addTurtle(CCRegistration.ID.OP_WEAK_AUTOMATA, "Overpowered weak automata");
+        addTurtle(CCRegistration.ID.OP_HUSBANDRY_AUTOMATA, "Overpowered husbandry automata");
+        addTurtle(CCRegistration.ID.OP_END_AUTOMATA, "Overpowered end automata");
 
     }
 
-    private void addTooltips() {
+    private void addPockets() {
+        addPocket(CCRegistration.ID.COLONY_POCKET, "Colony");
+        addPocket(CCRegistration.ID.CHATTY_POCKET, "Chatty");
+        addPocket(CCRegistration.ID.ENVIRONMENT_POCKET, "Environment");
+        addPocket(CCRegistration.ID.GEOSCANNER_POCKET, "Geo");
+        addPocket(CCRegistration.ID.PLAYER_POCKET, "Player Detector");
+    }
 
+    private void addAdvancements() {
+        addAdvancement("root", "Advanced Peripherals", "Every journey starts with the first block");
+        addAdvancement("weak_automata_core", "First automata core", "Does the afterlife exist in minecraft?");
+
+    }
+
+    private void addAdvancement(@NotNull String advancement, @NotNull String name, @NotNull String description) {
+        add("advancements." + AdvancedPeripherals.MOD_ID + "." + advancement, name);
+        add("advancements." + AdvancedPeripherals.MOD_ID + "." + advancement + ".description", description);
+    }
+
+    private void add(@NotNull Supplier<VillagerProfession> key, @NotNull String name) {
+        add("entity.minecraft.villager." + AdvancedPeripherals.MOD_ID + "." + key.get().name(), name);
+    }
+
+    private void addTurtle(@NotNull ResourceLocation key, @NotNull String name) {
+        add("turtle." + key.getNamespace() + "." + key.getPath(), name);
+    }
+
+    private void addPocket(@NotNull ResourceLocation key, @NotNull String name) {
+        add("pocket." + key.getNamespace() + "." + key.getPath(), name);
     }
 
 }
