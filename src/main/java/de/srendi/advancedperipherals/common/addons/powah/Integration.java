0c7b4937599efb948c8917b3deacb043f9499c6f
Sabry Chasseray <sabry.chasseray@protonmail.com>
2023-03-25 19:48:06 +0100

    Basic Powah Reactor Integration (fuel, carbon, redstone, energy, ...)

--- src/main/java/de/srendi/advancedperipherals/common/addons/powah/Integration.java
+++ src/main/java/de/srendi/advancedperipherals/common/addons/powah/Integration.java
@@ -1,11 +1,11 @@
 package de.srendi.advancedperipherals.common.addons.powah;
 
 import de.srendi.advancedperipherals.common.addons.computercraft.integrations.IntegrationPeripheralProvider;
-import owmii.powah.block.reactor.ReactorTile;
+import owmii.powah.block.reactor.ReactorPartTile;
 
 public class Integration implements Runnable {
     @Override
     public void run() {
-        IntegrationPeripheralProvider.registerBlockEntityIntegration(ReactorIntegration::new, ReactorTile.class);
+        IntegrationPeripheralProvider.registerBlockEntityIntegration(ReactorIntegration::new, ReactorPartTile.class);
     }
 }
--- src/main/java/de/srendi/advancedperipherals/common/addons/powah/ReactorIntegration.java
+++ src/main/java/de/srendi/advancedperipherals/common/addons/powah/ReactorIntegration.java
@@ -3,9 +3,9 @@ package de.srendi.advancedperipherals.common.addons.powah;
 import dan200.computercraft.api.lua.LuaFunction;
 import de.srendi.advancedperipherals.lib.peripherals.BlockEntityIntegrationPeripheral;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import owmii.powah.block.reactor.ReactorTile;
+import owmii.powah.block.reactor.ReactorPartTile;
 
-public class ReactorIntegration extends BlockEntityIntegrationPeripheral<ReactorTile> {
+public class ReactorIntegration extends BlockEntityIntegrationPeripheral<ReactorPartTile> {
     protected ReactorIntegration(BlockEntity entity) {
         super(entity);
     }
@@ -22,6 +22,36 @@ public class ReactorIntegration extends BlockEntityIntegrationPeripheral<Reactor
 
     @LuaFunction(mainThread = true)
     public final boolean isRunning() {
-        return blockEntity.isRunning();
+        return blockEntity.core().get().isRunning();
+    }
+
+    @LuaFunction(mainThread = true)
+    public final double getFuel() {
+        return blockEntity.core().get().fuel.perCent();
+    }
+
+    @LuaFunction(mainThread = true)
+    public final double getCarbon() {
+        return blockEntity.core().get().carbon.perCent();
+    }
+
+    @LuaFunction(mainThread = true)
+    public final double getRedstone() {
+        return blockEntity.core().get().redstone.perCent();
+    }
+
+    @LuaFunction(mainThread = true)
+    public final double getEnergy() {
+        return blockEntity.core().get().getEnergy().getEnergyStored();
+    }
+
+    @LuaFunction(mainThread = true)
+    public final double getMaxEnergy() {
+        return blockEntity.core().get().getEnergy().getMaxEnergyStored();
+    }
+
+    @LuaFunction(mainThread = true)
+    public final double getTemperature() {
+        return blockEntity.core().get().temp.perCent();
     }
 }
