febe05210a14eabee5ce8e714204fdd00b386438
Sabry Chasseray <sabry.chasseray@protonmail.com>
2023-03-26 18:34:18 +0200

    Powah Energy cell and solar panel integration

--- src/main/java/de/srendi/advancedperipherals/common/addons/powah/Integration.java
+++ src/main/java/de/srendi/advancedperipherals/common/addons/powah/Integration.java
@@ -1,11 +1,15 @@
 package de.srendi.advancedperipherals.common.addons.powah;
 
 import de.srendi.advancedperipherals.common.addons.computercraft.integrations.IntegrationPeripheralProvider;
+import owmii.powah.block.energycell.EnergyCellTile;
 import owmii.powah.block.reactor.ReactorPartTile;
+import owmii.powah.block.solar.SolarTile;
 
 public class Integration implements Runnable {
     @Override
     public void run() {
         IntegrationPeripheralProvider.registerBlockEntityIntegration(ReactorIntegration::new, ReactorPartTile.class);
+        IntegrationPeripheralProvider.registerBlockEntityIntegration(EnergyCellIntegration::new, EnergyCellTile.class);
+        IntegrationPeripheralProvider.registerBlockEntityIntegration(SolarPanelIntegration::new, SolarTile.class);
     }
 }
