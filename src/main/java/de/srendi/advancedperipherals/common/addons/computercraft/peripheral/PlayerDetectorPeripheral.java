e3833608efc25b825165e0ee192cbc8f185baef9
Michele Grifa <grifamichele@gmail.com>
2023-05-31 10:10:40 +0200

    Changed the player detector event listener to reflect existing chatbox behaviour

--- src/main/java/de/srendi/advancedperipherals/common/addons/computercraft/peripheral/PlayerDetectorPeripheral.java
+++ src/main/java/de/srendi/advancedperipherals/common/addons/computercraft/peripheral/PlayerDetectorPeripheral.java
@@ -17,8 +17,6 @@ import de.srendi.advancedperipherals.lib.peripherals.BasePeripheral;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.server.ServerLifecycleHooks;
 
 import java.util.*;
@@ -29,26 +27,14 @@ public class PlayerDetectorPeripheral extends BasePeripheral<IPeripheralOwner> {
 
     public PlayerDetectorPeripheral(PeripheralBlockEntity<?> tileEntity) {
         super(PERIPHERAL_TYPE, new BlockEntityPeripheralOwner<>(tileEntity));
-
-        registerListeners();
     }
 
     public PlayerDetectorPeripheral(ITurtleAccess access, TurtleSide side) {
         super(PERIPHERAL_TYPE, new TurtlePeripheralOwner(access, side));
-
-        registerListeners();
     }
 
     public PlayerDetectorPeripheral(IPocketAccess pocket) {
         super(PERIPHERAL_TYPE, new PocketPeripheralOwner(pocket));
-
-        registerListeners();
-    }
-
-    private void registerListeners() {
-        MinecraftForge.EVENT_BUS.addListener(this::onPlayerJoin);
-        MinecraftForge.EVENT_BUS.addListener(this::onPlayerLeave);
-        MinecraftForge.EVENT_BUS.addListener(this::onPlayerChangedDimension);
     }
 
     @Override
@@ -186,21 +172,5 @@ public class PlayerDetectorPeripheral extends BasePeripheral<IPeripheralOwner> {
 
     private List<ServerPlayer> getPlayers() {
         return ServerLifecycleHooks.getCurrentServer().getPlayerList().getPlayers();
-    }
-
-    private void onPlayerJoin(PlayerEvent.PlayerLoggedInEvent event) {
-        getConnectedComputers().forEach(computer -> computer.queueEvent("playerJoin", event.getEntity().getDisplayName().toString()));
-    }
-
-    private void onPlayerLeave(PlayerEvent.PlayerLoggedOutEvent event) {
-        getConnectedComputers().forEach(computer -> computer.queueEvent("playerLeave", event.getEntity().getDisplayName().toString()));
-    }
-
-    private void onPlayerChangedDimension(PlayerEvent.PlayerChangedDimensionEvent event) {
-        Player player = event.getEntity();
-        String fromDim = event.getFrom().location().toString();
-        String toDim = event.getTo().location().toString();
-
-        getConnectedComputers().forEach(computer -> computer.queueEvent("playerChangedDimension", player.getDisplayName().toString(), fromDim, toDim));
     }
 }
