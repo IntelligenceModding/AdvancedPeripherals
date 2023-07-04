f1b2f497a75d67b64e959fc4e48ea7ccb43ca13f
ThievingSix <3247540+Apeopex@users.noreply.github.com>
2023-07-03 20:47:16 -0700

    Add Powah's Ender Cell support
    - Includes energy reading (current, max)
    - Includes channel reading (current, max)
    - Include channel setting
    
    Channel support forced into 1-index based to match more conventional MC and Lua conventions.

---
+++ src/main/java/de/srendi/advancedperipherals/common/addons/powah/EnderCellIntegration.java
@@ -0,0 +1,53 @@
+package de.srendi.advancedperipherals.common.addons.powah;
+
+import dan200.computercraft.api.lua.LuaFunction;
+import de.srendi.advancedperipherals.lib.peripherals.BlockEntityIntegrationPeripheral;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import org.jetbrains.annotations.NotNull;
+import owmii.powah.block.ender.EnderCellTile;
+
+public class EnderCellIntegration extends BlockEntityIntegrationPeripheral<EnderCellTile> {
+    protected EnderCellIntegration(BlockEntity entity) {
+        super(entity);
+    }
+
+    @NotNull
+    @Override
+    public String getType() {
+        return "enderCell";
+    }
+
+    @LuaFunction(mainThread = true)
+    public final String getName() {
+        return "Ender Cell";
+    }
+
+    @LuaFunction(mainThread = true)
+    public final double getEnergy() {
+        return blockEntity.getEnergy().getEnergyStored();
+    }
+
+    @LuaFunction(mainThread = true)
+    public final double getMaxEnergy() {
+        return blockEntity.getEnergy().getMaxEnergyStored();
+    }
+
+    @LuaFunction(mainThread = true)
+    public final int getChannel() {
+        // Lua, and generally slots in MC, seem to be 1 based, make the conversion here
+        int channel = blockEntity.getChannel().get();
+        return channel + 1;
+    }
+
+    @LuaFunction(mainThread = true)
+    public final void setChannel(int channel) {
+        // Lua, and generally slots in MC, seem to be 1 based, make the conversion here
+        channel = channel - 1;
+        blockEntity.getChannel().set(channel);
+    }
+
+    @LuaFunction(mainThread = true)
+    public final int getMaxChannels() {
+        return blockEntity.getMaxChannels();
+    }
+}
