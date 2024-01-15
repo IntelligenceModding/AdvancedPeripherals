4468966667c4cf36a13fe4895fbcb08f3f8a4ef6
lonevox <quinn.epicness@gmail.com>
2024-01-15 14:22:57 +1300

    Change tabs to spaces

--- src/main/java/de/srendi/advancedperipherals/common/util/EntityUtil.java
+++ src/main/java/de/srendi/advancedperipherals/common/util/EntityUtil.java
@@ -9,35 +9,35 @@ import java.util.Map;
 import java.util.UUID;
 
 public class EntityUtil {
-	/**
-	 * A version of {@link EntityUtil#getEntityFromUUID} designed to be called from a
-	 * {@link dan200.computercraft.api.lua.LuaFunction LuaFunction}.
-	 * @param uuidList list containing 4 integers that together make a {@code UUID}
-	 * @return an {@code Entity} that has the given {@code UUID} if one is found
-	 * @throws LuaException throws if {@code uuidList} is incorrectly formatted or no {@code Entity} exists with the
-	 * given {@code UUID}
-	 */
-	public static Entity getEntityFromUUIDLua(Map<?, ?> uuidList) throws LuaException {
-		try {
-			UUID uuid = LuaConverter.luaToUUID(uuidList);
-			return getEntityFromUUID(uuid);
-		} catch (IllegalArgumentException e) {
-			throw new LuaException(e.getMessage());
-		}
-	}
+    /**
+     * A version of {@link EntityUtil#getEntityFromUUID} designed to be called from a
+     * {@link dan200.computercraft.api.lua.LuaFunction LuaFunction}.
+     * @param uuidList list containing 4 integers that together make a {@code UUID}
+     * @return an {@code Entity} that has the given {@code UUID} if one is found
+     * @throws LuaException throws if {@code uuidList} is incorrectly formatted or no {@code Entity} exists with the
+     * given {@code UUID}
+     */
+    public static Entity getEntityFromUUIDLua(Map<?, ?> uuidList) throws LuaException {
+        try {
+            UUID uuid = LuaConverter.luaToUUID(uuidList);
+            return getEntityFromUUID(uuid);
+        } catch (IllegalArgumentException e) {
+            throw new LuaException(e.getMessage());
+        }
+    }
 
-	/**
-	 * Searches all levels for an {@link Entity} with the given {@link UUID}.
-	 * @param uuid the unique identifier of the {@code Entity}
-	 * @return an {@code Entity} that has the given {@code UUID} if one is found
-	 * @throws IllegalArgumentException throws if there is no {@code Entity} with the given {@code UUID}
-	 */
-	public static Entity getEntityFromUUID(UUID uuid) {
-		for (ServerLevel level : ServerLifecycleHooks.getCurrentServer().getAllLevels()) {
-			Entity entity = level.getEntity(uuid);
-			if (entity != null)
-				return entity;
-		}
-		throw new IllegalArgumentException("No Entity with UUID '" + uuid + "' exists in any level");
-	}
+    /**
+     * Searches all levels for an {@link Entity} with the given {@link UUID}.
+     * @param uuid the unique identifier of the {@code Entity}
+     * @return an {@code Entity} that has the given {@code UUID} if one is found
+     * @throws IllegalArgumentException throws if there is no {@code Entity} with the given {@code UUID}
+     */
+    public static Entity getEntityFromUUID(UUID uuid) {
+        for (ServerLevel level : ServerLifecycleHooks.getCurrentServer().getAllLevels()) {
+            Entity entity = level.getEntity(uuid);
+            if (entity != null)
+                return entity;
+        }
+        throw new IllegalArgumentException("No Entity with UUID '" + uuid + "' exists in any level");
+    }
 }
