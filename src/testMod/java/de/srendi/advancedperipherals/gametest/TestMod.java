866474fb4ee6b11b2a5979c579da8efcbc514cac
Srendi <info@denizdemiroglu.de>
2024-02-23 22:53:16 +0100

    Add some testing game tests, next step is trying to run tests on computers with CC's testing api Removed some old stuff

---
+++ src/testMod/java/de/srendi/advancedperipherals/gametest/TestMod.java
@@ -0,0 +1,17 @@
+package de.srendi.advancedperipherals.gametest;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod("advancedperipheralstest")
+public class TestMod {
+
+    public TestMod() {
+    }
+
+    private static void onInitializeClient() {
+    }
+}
