cfc667edb23d39031d6418cb3cee364e05810f82
Srendi <info@denizdemiroglu.de>
2024-02-23 23:25:00 +0100

    Remove unused imports

--- src/testMod/java/de/srendi/advancedperipherals/gametest/TestMod.java
+++ src/testMod/java/de/srendi/advancedperipherals/gametest/TestMod.java
@@ -1,10 +1,6 @@
 package de.srendi.advancedperipherals.gametest;
 
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod("advancedperipheralstest")
 public class TestMod {
