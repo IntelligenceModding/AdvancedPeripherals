077a820ff01c3e5a23879a384593fbae6742b9c0
Jonathan Coates <git@squiddev.cc>
2024-04-05 10:18:55 +0100

    Convert Create integrations to use generic peripherals

--- src/main/java/de/srendi/advancedperipherals/common/addons/create/FluidTankIntegration.java
+++ src/main/java/de/srendi/advancedperipherals/common/addons/create/FluidTankIntegration.java
@@ -2,28 +2,22 @@ package de.srendi.advancedperipherals.common.addons.create;
 
 import com.simibubi.create.content.fluids.tank.FluidTankBlockEntity;
 import dan200.computercraft.api.lua.LuaFunction;
-import de.srendi.advancedperipherals.lib.peripherals.BlockEntityIntegrationPeripheral;
-import net.minecraft.world.level.block.entity.BlockEntity;
+import de.srendi.advancedperipherals.lib.peripherals.APGenericPeripheral;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class FluidTankIntegration extends BlockEntityIntegrationPeripheral<FluidTankBlockEntity> {
-
-    public FluidTankIntegration(BlockEntity entity) {
-        super(entity);
-    }
-
+public class FluidTankIntegration implements APGenericPeripheral {
     @NotNull
     @Override
-    public String getType() {
+    public String getPeripheralType() {
         return "fluidTank";
     }
 
     @LuaFunction(mainThread = true)
-    public final Map<String, Object> getInfo() {
+    public final Map<String, Object> getInfo(FluidTankBlockEntity blockEntity) {
         Map<String, Object> data = new HashMap<>();
         data.put("capacity", blockEntity.getControllerBE().getTankInventory().getCapacity());
         data.put("amount", blockEntity.getControllerBE().getTankInventory().getFluidAmount());
--- src/main/java/de/srendi/advancedperipherals/common/addons/create/Integration.java
+++ src/main/java/de/srendi/advancedperipherals/common/addons/create/Integration.java
@@ -1,20 +1,16 @@
 package de.srendi.advancedperipherals.common.addons.create;
 
-import com.simibubi.create.content.fluids.tank.FluidTankBlockEntity;
-import com.simibubi.create.content.kinetics.mixer.MechanicalMixerBlockEntity;
-import com.simibubi.create.content.processing.basin.BasinBlockEntity;
-import com.simibubi.create.content.processing.burner.BlazeBurnerBlockEntity;
-import de.srendi.advancedperipherals.common.addons.computercraft.integrations.IntegrationPeripheralProvider;
+import dan200.computercraft.api.ComputerCraftAPI;
 
 public class Integration implements Runnable {
 
     @Override
     public void run() {
-        IntegrationPeripheralProvider.registerBlockEntityIntegration(BlazeBurnerIntegration::new, BlazeBurnerBlockEntity.class);
-        IntegrationPeripheralProvider.registerBlockEntityIntegration(FluidTankIntegration::new, FluidTankBlockEntity.class);
+        ComputerCraftAPI.registerGenericSource(new BlazeBurnerIntegration());
+        ComputerCraftAPI.registerGenericSource(new FluidTankIntegration());
         // Disable until verified that it does not clash with the existing create CC integration
         //IntegrationPeripheralProvider.registerBlockEntityIntegration(ScrollValueBehaviourIntegration::new, KineticTileEntity.class, tile -> tile.getBehaviour(ScrollValueBehaviour.TYPE) != null, 10);
-        IntegrationPeripheralProvider.registerBlockEntityIntegration(BasinIntegration::new, BasinBlockEntity.class);
-        IntegrationPeripheralProvider.registerBlockEntityIntegration(MechanicalMixerIntegration::new, MechanicalMixerBlockEntity.class);
+        ComputerCraftAPI.registerGenericSource(new BasinIntegration());
+        ComputerCraftAPI.registerGenericSource(new MechanicalMixerIntegration());
     }
 }
