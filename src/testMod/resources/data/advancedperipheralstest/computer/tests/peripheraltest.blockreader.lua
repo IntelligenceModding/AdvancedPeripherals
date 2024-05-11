7f9af5c36290acde0231a4ae0e6aefe914955148
Dogboy21 <info@dogboy.io>
2024-05-11 17:15:45 +0200

    tablelength -> tableLength

--- src/testMod/resources/data/advancedperipheralstest/computer/tests/peripheraltest.blockreader.lua
+++ src/testMod/resources/data/advancedperipheralstest/computer/tests/peripheraltest.blockreader.lua
@@ -3,7 +3,7 @@
 --- Covers `getBlockName`, `getBlockData`, `getBlockStates`, `isTileEntity`
 ---
 
-function tablelength(T)
+function tableLength(T)
   local count = 0
   for _ in pairs(T) do count = count + 1 end
   return count
@@ -17,7 +17,7 @@ test.assert(simpleReader, "Peripheral not found")
 test.eq("minecraft:polished_andesite", simpleReader.getBlockName(), "Block Name should be polished_andesite")
 test.eq(nil, simpleReader.getBlockData(), "Block Data should be nil")
 test.assert(not simpleReader.isTileEntity(), "Block should not be a TileEntity")
-test.eq(0, tablelength(simpleReader.getBlockStates()), "Block State should be empty")
+test.eq(0, tableLength(simpleReader.getBlockStates()), "Block State should be empty")
 
 -- Test Block Reader functions on a stair block
 test.eq("blockReader", peripheral.getType("back"), "Peripheral should be blockReader")
@@ -27,7 +27,7 @@ test.assert(stairReader, "Peripheral not found")
 test.eq("minecraft:polished_andesite_stairs", stairReader.getBlockName(), "Block Name should be polished_andesite")
 test.eq(nil, stairReader.getBlockData(), "Block Data should be nil")
 test.assert(not stairReader.isTileEntity(), "Block should not be a TileEntity")
-test.eq(4, tablelength(stairReader.getBlockStates()), "Block State should not be empty")
+test.eq(4, tableLength(stairReader.getBlockStates()), "Block State should not be empty")
 
 test.eq("east", stairReader.getBlockStates()["facing"], "Stair Facing should be east")
 test.eq("bottom", stairReader.getBlockStates()["half"], "Stair Half should be bottom")
@@ -42,7 +42,7 @@ test.assert(signReader, "Peripheral not found")
 test.eq("minecraft:oak_sign", signReader.getBlockName(), "Block Name should be polished_andesite")
 test.neq(nil, signReader.getBlockData(), "Block Data should not be nil")
 test.assert(signReader.isTileEntity(), "Block should be a TileEntity")
-test.eq(2, tablelength(signReader.getBlockStates()), "Block State should not be empty")
+test.eq(2, tableLength(signReader.getBlockStates()), "Block State should not be empty")
 
 test.eq(4, signReader.getBlockStates()["rotation"], "Sign Rotation should be 4")
 test.assert(not signReader.getBlockStates()["waterlogged"], "Sign Waterlogged should be false")
