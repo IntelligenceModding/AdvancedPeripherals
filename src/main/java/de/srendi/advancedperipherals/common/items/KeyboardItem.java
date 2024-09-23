027787b14537f12686118b7e9c90fd80cba6d19a
endi <info@denizdemiroglu.de>
2024-09-23 21:21:40 +0200

    Binding/Unbinding for the keyboard

--- src/main/java/de/srendi/advancedperipherals/common/items/KeyboardItem.java
+++ src/main/java/de/srendi/advancedperipherals/common/items/KeyboardItem.java
@@ -1,17 +1,30 @@
 package de.srendi.advancedperipherals.common.items;
 
+import dan200.computercraft.shared.computer.blocks.TileComputerBase;
 import de.srendi.advancedperipherals.common.container.KeyboardContainer;
 import de.srendi.advancedperipherals.common.items.base.BaseItem;
 import de.srendi.advancedperipherals.common.items.base.IInventoryItem;
+import de.srendi.advancedperipherals.common.util.NBTUtil;
+import de.srendi.advancedperipherals.common.util.SideHelper;
+import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import org.jetbrains.annotations.NotNull;
 
 public class KeyboardItem extends BaseItem implements IInventoryItem {
+
+    private static final String BIND_TAG = "bind";
 
     public KeyboardItem() {
         super(new Properties().stacksTo(1));
@@ -20,6 +33,49 @@ public class KeyboardItem extends BaseItem implements IInventoryItem {
     @Override
     public boolean isEnabled() {
         return true;
+    }
+
+    @NotNull
+    @Override
+    public InteractionResult useOn(UseOnContext context) {
+        if (context.getPlayer() == null)
+            return InteractionResult.PASS;
+
+        if (SideHelper.isClientPlayer(context.getPlayer()))
+            return InteractionResult.PASS;
+
+        if (!context.getPlayer().isShiftKeyDown())
+            return InteractionResult.PASS;
+
+        BlockEntity entity = context.getLevel().getBlockEntity(context.getClickedPos());
+        if (entity instanceof TileComputerBase) {
+            bind(context.getPlayer(), context.getItemInHand(), context.getClickedPos());
+        } else {
+            clear(context.getPlayer(), context.getItemInHand());
+        }
+        return super.useOn(context);
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level worldIn, Player playerIn, InteractionHand handIn) {
+        // Used to prevent the menu from opening when we just want to bind/unbind the keyboard
+        if (!playerIn.isShiftKeyDown())
+            return super.use(worldIn, playerIn, handIn);
+
+        return new InteractionResultHolder<>(InteractionResult.PASS, playerIn.getItemInHand(handIn));
+    }
+
+    private void bind(Player player, ItemStack itemStack, BlockPos pos) {
+        CompoundTag tag = NBTUtil.toNBT(pos);
+
+        itemStack.getOrCreateTag().put(BIND_TAG, tag);
+        player.sendSystemMessage(Component.literal("Bound to " + pos.toShortString()));
+    }
+
+    private void clear(Player player, ItemStack itemStack) {
+        itemStack.getOrCreateTag().remove(BIND_TAG);
+
+        player.sendSystemMessage(Component.literal("Unbound"));
     }
 
     @Override
