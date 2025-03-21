3ee0bfa4d8d9ddc309fdd369a1dd49d3f074fdda
Srendi <info@denizdemiroglu.de>
2025-03-21 17:50:39 +0100

    Uncomment forgotten jobStateChanged from our TaskStatusListener the bridge to listen for task changes

--- src/main/java/de/srendi/advancedperipherals/common/blocks/blockentities/RsBridgeEntity.java
+++ src/main/java/de/srendi/advancedperipherals/common/blocks/blockentities/RsBridgeEntity.java
@@ -114,17 +114,17 @@ public class RsBridgeEntity extends PeripheralBlockEntity<RSBridgePeripheral> im
 
     @Override
     public void taskStatusChanged(@NotNull TaskStatus taskStatus) {
-        //jobs.stream().filter(job -> job.isCraftingStarted() && job.getCraftingTask().info().id().equals(taskStatus.info().id())).forEach(BasicCraftJob::jobStateChanged);
+        jobs.stream().filter(job -> job.isCraftingStarted() && job.getCraftingTask().info().id().equals(taskStatus.info().id())).forEach(BasicCraftJob::jobStateChanged);
     }
 
     @Override
     public void taskRemoved(@NotNull TaskId taskId) {
-        //jobs.stream().filter(job -> job.isCraftingStarted() && job.getCraftingTask().info().id().equals(taskId)).forEach(BasicCraftJob::jobStateChanged);
+        jobs.stream().filter(job -> job.isCraftingStarted() && job.getCraftingTask().info().id().equals(taskId)).forEach(BasicCraftJob::jobStateChanged);
 
     }
 
     @Override
     public void taskAdded(@NotNull TaskStatus taskStatus) {
-        //jobs.stream().filter(job -> job.isCraftingStarted() && job.getCraftingTask().info().id().equals(taskStatus.info().id())).forEach(BasicCraftJob::jobStateChanged);
+        jobs.stream().filter(job -> job.isCraftingStarted() && job.getCraftingTask().info().id().equals(taskStatus.info().id())).forEach(BasicCraftJob::jobStateChanged);
     }
 }
