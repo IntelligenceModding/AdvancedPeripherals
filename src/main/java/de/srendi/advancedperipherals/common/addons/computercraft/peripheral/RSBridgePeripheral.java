93485152d3c099060f64e0c49fb010e8e3a8747e
Srendi <info@denizdemiroglu.de>
2025-04-18 23:40:27 +0200

    More nested if statements
    Co-authored-by: Kevin Z <zyxkad@gmail.com>
    Signed-off-by: Srendi <info@denizdemiroglu.de>

--- src/main/java/de/srendi/advancedperipherals/common/addons/computercraft/peripheral/RSBridgePeripheral.java
+++ src/main/java/de/srendi/advancedperipherals/common/addons/computercraft/peripheral/RSBridgePeripheral.java
@@ -431,11 +431,12 @@ public class RSBridgePeripheral extends BasePeripheral<BlockEntityPeripheralOwne
 
         IItemHandler inventory = InventoryUtil.getHandlerFromDirection(arguments.getString(1), owner);
 
-        if (inventory == null)
+        if (inventory == null) {
             inventory = InventoryUtil.getHandlerFromName(computer, arguments.getString(1));
-
-        if (inventory == null)
-            return MethodResult.of(0, StatusConstants.INVENTORY_NOT_FOUND.name());
+            if (inventory == null) {
+                return MethodResult.of(0, StatusConstants.INVENTORY_NOT_FOUND.name());
+            }
+        }
 
         return exportToChest(arguments, inventory);
     }
