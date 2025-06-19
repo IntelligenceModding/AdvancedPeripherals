b471dd375037905bdf1ef4c1badb5b1982b0cde1
zyxkad <zyxkad@gmail.com>
2025-06-18 21:35:18 -0600

    remove unused import

--- src/main/java/de/srendi/advancedperipherals/common/items/KeyboardItem.java
+++ src/main/java/de/srendi/advancedperipherals/common/items/KeyboardItem.java
@@ -19,7 +19,6 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.Component;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
