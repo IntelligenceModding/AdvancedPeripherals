e38d6940d45ab70add5133a780eb1d7e4b4e703d
zyxkad <zyxkad@gmail.com>
2026-09-04 17:46:55 -0600

    update zh_cn translation and recipe

--- src/main/java/de/srendi/advancedperipherals/common/data/RecipesProvider.java
+++ src/main/java/de/srendi/advancedperipherals/common/data/RecipesProvider.java
@@ -279,7 +279,7 @@ public class RecipesProvider extends RecipeProvider implements IConditionBuilder
         ShapedRecipeBuilder.shaped(RecipeCategory.REDSTONE, APBlocks.DISTANCE_DETECTOR.get())
             .define('C', CASING)
             .define('O', Items.OBSERVER)
-            .define('D', Tags.Items.GEMS_DIAMOND)
+            .define('D', Items.AMETHYST_SHARD)
             .define('R', Tags.Items.STORAGE_BLOCKS_REDSTONE)
             .define('G', Tags.Items.GLASS)
             .pattern("GDG")
