bf0db4dc383c48580750034703f7a7029bfb865f
zyxkad <zyxkad@gmail.com>
2026-09-08 12:55:52 -0600

    checkstyle

--- src/main/java/de/srendi/advancedperipherals/common/data/RecipesProvider.java
+++ src/main/java/de/srendi/advancedperipherals/common/data/RecipesProvider.java
@@ -24,8 +24,6 @@ import net.minecraft.data.recipes.ShapedRecipeBuilder;
 import net.minecraft.data.recipes.ShapelessRecipeBuilder;
 import net.minecraft.data.recipes.SmithingTransformRecipeBuilder;
 import net.minecraft.tags.ItemTags;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.alchemy.Potion;
 import net.minecraft.world.item.alchemy.PotionContents;
